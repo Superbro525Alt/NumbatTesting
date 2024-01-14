f1a8988fa9dc85e1fd830e7f727cdf183248a3bb
Isaac Turner <spacey_sooty@outlook.com>
2024-01-14 11:26:53 +0800

    Removed Java, Drivers; Added formatting and linting

--- src/main/native/include/Numbat.h
+++ src/main/native/include/Numbat.h
@@ -2,9 +2,6 @@
 // Open Source Software, you can modify it according to the terms
 // of the MIT License at the root of this project
 
-#include "gtest/gtest.h"
+#pragma once
 
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+namespace numbat {}  // namespace numbat
